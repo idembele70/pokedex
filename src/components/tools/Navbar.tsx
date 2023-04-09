042e7be02f28691e74c08a99f3c83c2422aa6651
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-04-09 03:47:45 +0200

    [Header] I have simplified the header code.

--- src/components/tools/Navbar.tsx
+++ src/components/tools/Navbar.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { mobile } from "../../utils/responsive";
 
 const Container = styled.header`
-  margin-top: 30px;
-  margin-bottom: 30px;
-  flex-grow: 90px;
+  padding: 30px 0;
   width: 80vw;
   max-width: 460px;
+`;
+
+const Nav = styled.nav`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
@@ -16,12 +16,9 @@ const Container = styled.header`
   ${mobile({ maxWidth: 308, gap: "10px 25px" })}
 `;
 
-interface ButtonProps {
-  isactive: boolean;
-}
-
-const Button = styled.nav<ButtonProps>`
+const NavItem = styled(NavLink)`
   cursor: pointer;
+  text-decoration: none;
   font-size: 14px;
   line-height: 16px;
   font-weight: 700;
@@ -36,33 +33,20 @@ const Button = styled.nav<ButtonProps>`
     background-color: ${({ theme }) => theme.palette.secondary.main};
     box-shadow: 0 4px 16px -3px ${({ theme }) => theme.palette.common.white};
   }
-  box-shadow: ${({ isactive, theme }) =>
-    isactive ? `0px 4px 16px -3px ${theme.palette.secondary.main}` : ""};
+  &.active {
+    box-shadow: ${({ theme }) =>
+      `0px 4px 16px -3px ${theme.palette.secondary.main}`};
+  }
   ${mobile({ padding: "7px 8%" })}
 `;
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const handleNavigate = (link: string) => {
-    navigate(`/pokedex/${link}`);
-  };
-  const [isActive, setIsActive] = useState("home");
-  useEffect(() => {
-    if (location.pathname === "/") setIsActive("home");
-    else setIsActive("liked");
-  }, [location]);
   return (
     <Container>
-      <Button isactive={isActive === "home"} onClick={() => handleNavigate("")}>
-        Pokedex
-      </Button>
-      <Button
-        isactive={isActive === "liked"}
-        onClick={() => handleNavigate("liked")}
-      >
-        Liked
-      </Button>
+      <Nav>
+        <NavItem to="/">Pokedex</NavItem>
+        <NavItem to="/liked">Liked</NavItem>
+      </Nav>
     </Container>
   );
 };
